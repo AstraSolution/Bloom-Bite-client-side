a2773deba2ea012226f48a43c0cabf9f793d21fb
ShahreyarRafi <shahreyar.rafi@gmail.com>
2024-01-14 20:09:22 +0600

    deleted test.jsx

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,7 +1,6 @@
 import { useLoaderData } from 'react-router-dom';
 import Banner from '../../Components/Header/Banner/Banner'
 import WhyUs from '../../Components/WhyUs/WhyUs';
-import Test from '../../Components/test/test';
 
 
 const Home = () => {
@@ -9,7 +8,6 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <Test></Test>
             <WhyUs></WhyUs>
         </div>
     );
