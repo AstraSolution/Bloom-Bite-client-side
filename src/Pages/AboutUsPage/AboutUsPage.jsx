ffc06fb529a79cffce43f813d28126d9357c0c3e
Adriyan-Ahmed <adriyan.ahmed.jubayer@gmail.com>
2024-02-13 15:27:14 +0600

    Our Story Section Created & Added in About Us Page

--- src/Pages/AboutUsPage/AboutUsPage.jsx
+++ src/Pages/AboutUsPage/AboutUsPage.jsx
@@ -11,6 +11,7 @@ import { MdOutlineInsertInvitation } from "react-icons/md";
 import TitleBanner from "../../Shared/Title Banner/TitleBanner";
 import Construction from "../../Shared/Construction/Construction";
 import Testimonial from "../../Components/Testimonial/Testimonial";
+import OurStory from "../../Components/OurStory/OurStory";
 
 const AboutUsPage = () => {
     return (
@@ -18,17 +19,8 @@ const AboutUsPage = () => {
             <section className="mx-auto font-primary">
                 <TitleBanner Title={`ABOUT US`} Track={`HOME > ABOUT`}></TitleBanner>
             </section>
-            <section className="flex flex-col lg:flex-row justify-center items-center font-primary mt-14 md:mt-20 mx-4 gap-9">
-                <div className="flex-1 space-y-6">
-                    <h1 className="text-center lg:text-left text-xl md:text-2xl lg:text-3xl font-bold">OUR STORY OF FOOD</h1>
-                    <p className="text-[12px] md:text-[14px] lg:text-[16px] leading-6 md:leading-7 lg:leading-9">Pleased anxious or as in by viewing forbade minutes prevent. Too leave had those get being led weeks blind. Had men rose from down lady able. Its son him ferrars proceed six parlors.</p>
-                    <p className="text-[12px] md:text-[14px] lg:text-[16px] leading-6 md:leading-7 lg:leading-9">Advanced diverted domestic sex repeated bringing you old. Possible procured her trifling laughter thoughts property she met way. Companions shy had solicitude favourable own. Which could saw guest man now heard but. Lasted my coming uneasy marked so should. Gravity letters it amongst herself dearest an windows by. Wooded ladies she basket.</p>
-                    <p className="text-[12px] md:text-[14px] lg:text-[16px] leading-6 md:leading-7 lg:leading-9">There are many variations of passages of Lorem Ipsum available, but the to a majority have suffered alteration in some form, by injected humour, or find randomised words which don't look even slightly believable.</p>
-                    <p className="text-[12px] md:text-[14px] lg:text-[16px] leading-6 md:leading-7 lg:leading-9">Over 20 years’ experience providing top quality house Booking rant and sell for your Amazing Dream & Make you Happy.</p>
-                </div>
-                <div className="flex-1">
-                    <img className="mx-auto" src="https://quomodosoft.com/html/reservq/assets/images/thumb/about-thumb.png" alt="" />
-                </div>
+            <section>
+                <OurStory></OurStory>
             </section>
             <section className="mt-20 md:mt-20 lg:mt-24 mx-4 font-primary space-y-4 lg:space-y-6">
                 <h1 className="text-center text-xl md:text-2xl lg:text-3xl font-bold">WHY CHOOSE US</h1>
